ae6e9630700854a56d09960aac035d5b595a40e4
Galiant <antonijodev@gmail.com>
2019-03-07 22:35:25 +0000

    Bug fixed - menu working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,3 +10,22 @@ $('.typed-title').typed({
   typeSpeed: 0,
   loop: true
 });
+
+// Function to change the background color of the menu button and the display value of the menu
+function accordionMenu() {
+  var menu = document.getElementById("toggled");
+  var menuStyle = window.getComputedStyle(menu);
+  var display = menuStyle.getPropertyValue('display');
+
+  if (display === 'none') {
+    toggle.style.backgroundColor = "gray";
+    menu.style.display = "block";
+  } else {
+    toggle.style.backgroundColor = "transparent";
+    menu.style.display = "none";
+  };
+};
+
+// Add event listener to menu button
+var toggle = document.getElementById("menu-toggle");
+toggle.addEventListener("click", accordionMenu, false);
